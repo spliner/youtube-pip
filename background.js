4ea65587fbf155aa1690f14ea1b9c7f30f6a1e13
Fillipe Rosini <filliperosini@gmail.com>
2013-07-24 17:50:34 -0300

    Initial implementation

---
+++ background.js
@@ -0,0 +1,73 @@
+chrome.tabs.onUpdated.addListener(onTabUpdated);
+chrome.pageAction.onClicked.addListener(onPageActionClicked);
+chrome.contextMenus.create({ title: 'Open in PIP', contexts: ['video', 'link'], onclick: function(info, tab) { onContextMenuClick(info); } });
+
+var defaultPanelHeight = 360;
+var defaultPanelWidth = 480;
+
+function onTabUpdated(tabId, changeInfo, tab) {
+	if (tab.url.indexOf('youtube.com') > -1 && tab.url.indexOf('watch?') > -1) {
+		chrome.pageAction.show(tab.id);
+		chrome.tabs.executeScript(tab.id, { file: 'contentscript.js' });
+	} else {
+		chrome.pageAction.hide(tab.id);
+	}
+}
+
+function onPageActionClicked(tab) {
+	chrome.tabs.getSelected(null, function(tab) {
+		chrome.tabs.sendMessage(tab.id, { greeting: 'PlayVideo' }, handleContentScriptResponse);
+	});
+}
+
+function handleContentScriptResponse(response) {
+	if (response) {
+		if (response.farewell.indexOf('Error') === -1) {
+			createVideoPanel(response.farewell);
+		}
+		else {
+			console.log('Error creating video: ' + response.farewell);
+		}
+	} else {
+		console.log('Error handling content script response: null response');
+	}
+}
+
+function onContextMenuClick(clickInfo) {
+	var url = clickInfo.linkUrl;
+	var playParameters = new Array();
+	if (url.indexOf('youtube.com') > -1 && (url.indexOf('/watch/') > -1 || url.indexOf('/embed/') >-1)) {
+		var videoId = '';
+		var listId = '';
+		if (url.indexOf('/embed/') > -1) {
+			videoId = url.split('/embed/')[1];
+			var parametersPosition = videoId.indexOf('?');
+			if (parametersPosition > -1)
+				videoId = videoId.subString(0, parametersPosition);
+			listId = getUrlVars(url)['list'];
+		} else {
+			videoId = getUrlVars(url)['v'];
+			listId = getUrlVars(url)['list'];
+		}
+		playParameters.push('video=' + videoId);
+		if (listId)
+			playParameters.push('list=' + listId);
+		createVideoPanel(playParameters.join('&'));
+	}
+}
+
+function createVideoPanel(parameters) {
+	var videoParameters = new Array();
+	videoParameters.push('height=240');
+	videoParameters.push('width=320');
+	videoParameters.push('autoplay=1');
+	chrome.windows.create({ url: 'player.html?' + parameters + '&' + videoParameters.join('&'), type: 'panel', height: defaultPanelHeight, width: defaultPanelWidth }, function(window) { window.alwaysOnTop = true; });
+}
+
+function getUrlVars(url) {
+    var vars = {};
+    var parts = url.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m, key, value) {
+        vars[key] = value;
+    });
+    return vars;
+}
---
+++ player.js
@@ -0,0 +1,36 @@
+window.onload = playVideo;
+
+//TODO: How to get video quality?
+function playVideo() {
+	var embedUrl = 'http://www.youtube.com/embed/';
+	var videoId = getUrlVars()['video'];
+	var time = getUrlVars()['time'];
+	var playlistId = getUrlVars()['list'];
+	var height = getUrlVars()['height'];
+	var width = getUrlVars()['width'];
+	var autoplay = getUrlVars()['autoplay'];
+
+	embedUrl += videoId;
+	embedUrl += '?autoplay=' + autoplay;
+	embedUrl += '&autohide=1';
+	if (playlistId)
+		embedUrl += '&list=' + playlistId;
+	if (time)
+		embedUrl += '&start=' + time;
+	
+	var playerContainer = document.getElementById('player-container');
+	var frame = document.createElement('iframe');
+	frame.setAttribute('id', 'player-frame');
+	frame.setAttribute('src', embedUrl);
+	frame.setAttribute('frameborder', '0');
+	frame.setAttribute('allowfullscreen', 'allowfullscreen');
+	playerContainer.appendChild(frame);	
+}
+
+function getUrlVars() {
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m, key, value) {
+        vars[key] = value;
+    });
+    return vars;
+}
